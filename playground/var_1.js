e354942d08d0f03b3b7ac9ef96b984e59b36e6e6
vincent-mugendi <vincentmwathi5@gmail.com>
2024-07-11 21:13:38 +0300

    practicing some variable manipulations

--- playground/var_1.js
+++ playground/var_1.js
@@ -1,6 +1,13 @@
 let firstName = 'Vincent';
 let lastName = "Mugendi";
 
+// concatenating
 let fullName = firstName + " " +lastName;
-console.log(fullName)
+console.log(fullName);
+
+// find the length os a string
+console.log(fullName.length);
+
+// find 3rd index of firstName
+console.log(firstName[3]);
 
