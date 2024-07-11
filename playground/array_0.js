a82e731541ae9243d987bc945769f5c3793befdb
vincent-mugendi <vincentmwathi5@gmail.com>
2024-07-12 00:01:31 +0300

    practicing some array manipulation

---
+++ playground/array_0.js
@@ -0,0 +1,20 @@
+let shopping = [
+    'mangoes',
+    'salt',
+    'sugar',
+    'butter'
+]
+
+console.log(shopping)
+
+// nesting array
+let nestedArray = [
+    [
+        'one', 'two', 'three'
+    ],
+    [
+        1, 2, 3
+    ]
+]
+
+console.log(nestedArray)
