86d7aa7a64a82a9eaeead67a75dde6d0ceeb337e
vincent-mugendi <vincentmwathi5@gmail.com>
2024-07-12 18:25:31 +0300

    updates on arrow_functions notes

---
+++ playground/arrow_function.js
@@ -0,0 +1,13 @@
+// practicing writing arrow functions
+const add = (a, b) => a + b;
+
+console.log(add(2, 8));
+
+
+// 1 parameter, square function
+const square = x => x * x;
+console.log(square(12));
+
+// no paramter, return hello
+const greeting = () => "hello";
+console.log(greeting());
---
+++ playground/arrow_function_0.js
@@ -0,0 +1,19 @@
+//   1. Convert the following traditional functions to arrow functions:
+
+// Traditional function:
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+
+const multiply = (a, b) => a * b;
+console.log(multiply(5, 5));
+
+
+// Traditional function:
+
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
+
+
