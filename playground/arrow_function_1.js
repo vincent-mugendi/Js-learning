f523e2505bfc41d289c9ba155514fcd40b978407
vincent-mugendi <vincentmwathi5@gmail.com>
2024-07-14 05:53:46 +0300

    messing around with some arrow functions

---
+++ playground/arrow_function_1.js
@@ -0,0 +1,53 @@
+// Traditional function:
+//       function add(a, b) {
+//         return a + b;
+//       }
+
+const add = (a, b) => a + b;
+console.log(add(2, 4));
+
+
+// Traditional function:
+//       function square(x) {
+//         return x * x;
+//       }
+
+const square = x => x * x;
+console.log(square(5));
+
+
+// Traditional function:
+//       function greet() {
+//         return 'Hello!';
+//       }
+
+const greet = () => 'Hello';
+console.log(greet());
+
+
+// Traditional function:
+//     function multiply(a, b) {
+//       return a * b;
+//     }
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 5));
+
+// Traditional function:
+//     function isEven(num) {
+//       return num % 2 === 0;
+//     }
+
+const isEven = num => num % 2 === 0;
+console.log(isEven(6594370));
+
+// Traditional function:
+//     function sayHello(name) {
+//       return `Hello, ${name}!`;
+//     }
+
+
+const sayHello = name => `Hello, ${name}.`;
+console.log(sayHello('Beats'));
+
+
