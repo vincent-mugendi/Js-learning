25b6ad5cacc90b1036990e9fe88ee9dc1b269b03
vincent-mugendi <vincentmwathi5@gmail.com>
2024-07-15 20:49:08 +0300

    updating javascript notes

---
+++ playground/object_0.js
@@ -0,0 +1,30 @@
+// create an object for a student
+const student = {
+    name: 'Vincent',
+    class: '3N',
+};
+console.log(student);
+
+// update student claas to 4H
+
+student.class = '4H';
+console.log(student)
+
+// add a key called gpa with a value 5.0
+student.gpa = 5.0;
+console.log(student)
+
+// delete class key pair
+delete student.class;
+console.log(student);
+
+// check if property name exists
+const checkProperty = (property) => {
+    if (student.hasOwnProperty(property))
+        console.log("Property present");
+    else
+        console.log('Property Absent');
+}
+
+checkProperty('class');
+
