c7ea051b86bda6ca38fd34a7b4c283858e38cf71
vincent-mugendi <vincentmwathi5@gmail.com>
2024-09-19 06:51:34 +0300

    literals playground

---
+++ playground/vanilla-js/literals-0.js
@@ -0,0 +1,22 @@
+// Using quotes
+
+var quote;
+quote = 'John said; \'What the f*ck are you talking about\'';
+console.log(quote);
+
+
+// backslash
+var sentence_0 = 'This is a \\ backslash';
+console.log(sentence_0);
+
+// newline
+var paragraph = 'This is first line. \nThis is the 2nd line.';
+console.log(paragraph);
+
+
+// using tab
+var paragraph_1;
+paragraph_1 = 'I love colors:\n\tred\n\tblude\n\tgreen'
+console.log(paragraph_1)
+
+
