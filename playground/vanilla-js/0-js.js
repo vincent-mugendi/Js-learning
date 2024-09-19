ea62db78b1c7e8c2156b62c6923cf329683433fa
vincent-mugendi <vincentmwathi5@gmail.com>
2024-09-19 06:21:40 +0300

    practicing some vanilla js

---
+++ playground/vanilla-js/0-js.js
@@ -0,0 +1,18 @@
+// Testing Js is case sensitive
+// Name is different from name
+
+var Name = 'John';
+var name = 'paul';
+
+console.log(Name)
+console.log(name)
+
+// strings are immutable, they cannot be altered once created
+Name[0] = 'p';
+console.log(Name)
+
+// This is a single line comment
+/* This
+is a multi-line
+comment */
+
