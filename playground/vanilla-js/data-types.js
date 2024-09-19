406037f884857303787a585a8a0abff5d2c66342
vincent-mugendi <vincentmwathi5@gmail.com>
2024-09-19 06:32:02 +0300

    practicing some data types

---
+++ playground/vanilla-js/data-types.js
@@ -0,0 +1,37 @@
+// Data Types
+// 1. undefined
+let name;
+
+console.log(name)
+
+
+// 2. null
+var value = null;
+console.log(value)
+
+// 3. boolean
+var login = true;
+var logout = false;
+console.log(login, logout)
+
+// 4. string
+var string = "Characters"
+console.log(string)
+
+// 5. Number
+var id = 456;
+console.log(id)
+
+// 6. floats
+var pi = 3.14
+console.log(pi)
+
+
+// 7. object
+var student = {
+    'name': 'John',
+    'age': 5
+}
+console.log(student)
+
+
