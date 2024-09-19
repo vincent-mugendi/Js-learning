743c442823add0d6cb5c9e688fe1c0ce9bebecb0
vincent-mugendi <vincentmwathi5@gmail.com>
2024-09-19 06:59:13 +0300

    playing with strings

---
+++ playground/vanilla-js/strings-0.js
@@ -0,0 +1,21 @@
+// concatenating strings
+var firstName = 'John';
+var lastName = 'fiddle';
+
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+let greet = 'Hello' + ' ' + firstName;
+console.log(greet)
+
+// Finding the length of a string
+console.log(firstName.length)
+
+
+// Whats the 2nd character in firstName
+console.log(firstName[1])
+
+// Whats the last character in firstName
+console.log(firstName[3]) //find the length and minus 1 from it
+
+// 
