11b1cec4701012c3d4308d10d1d5f8e2aae12983
vincent-mugendi <vincentmwathi5@gmail.com>
2024-09-20 07:07:10 +0300

    playing with if else statements

---
+++ playground/vanilla-js/if-else-0.js
@@ -0,0 +1,45 @@
+// use an if else statement
+
+var x = 23;
+
+if (x < 18){
+    console.log("You are underage")
+}
+
+else {
+    console.log("You are an adult")
+}
+
+// == :Equals operator
+if (x == 23){
+    console.log("You are 23");
+}
+// === :Strict equals operator
+if (x === 23){
+    console.log("You are really 23")
+}
+
+// != :Not Equals operator
+if (x != 24){
+    console.log("You are not 24")
+}
+// !== :Strict not equals operator
+if (x !== 24){
+    console.log("You are really not 24")
+}
+
+// > :Greator than
+if (x > 18){
+    console.log("Adult")
+}
+// < :Less than
+if (x < 25){
+    console.log("Youa are under 25")
+}
+
+// >= :Greator than or equal to
+
+// <= :Less than or equal to
+
+// && :and operator
+// || :OR operator
