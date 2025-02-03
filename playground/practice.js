a824187c30bb9b3af0d1f4af17afa6ff3ee92b88
vincent-mugendi <vincentmwathi5@gmail.com>
2025-02-03 18:26:48 +0300

    updating notes

---
+++ playground/practice.js
@@ -0,0 +1,16 @@
+// single line comment
+/* 
+multiline comment
+*/
+
+var name = "Vincent";
+// var pi = 3.14;
+
+console.log(typeof(name));
+console.log(typeof(pi))
+
+
+const pi = 3.14;
+pi = 3;
+
+console.log(pi);
---
+++ practice/dom.js
@@ -0,0 +1,7 @@
+document.getElementById("title").innerHTML = "Hello WorlD";
+
+document.getElementsByClassName("hero")[0].innerHTML = "WELCOME ABOARD";
+
+document.getElementsByTagName("h2")[0].innerHTML = "H2 TITLE";
+
+document.querySelector(".content h3").innerHTML = "Hi";
